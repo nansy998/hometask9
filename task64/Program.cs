a3e10167dedb3781f7cfaff533740e41e1445b2f
Nancy <nastyshazoloto@mail.ru>
2022-09-12 14:23:25 +0300

    task64

---
+++ task64/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = InputNumbers("Введите n: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
+
+void PrintNumber(int n, int count)
+{
+    if (count > n) return;
+    PrintNumber(n, count + 1);
+    Console.Write(count + ", ");
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
